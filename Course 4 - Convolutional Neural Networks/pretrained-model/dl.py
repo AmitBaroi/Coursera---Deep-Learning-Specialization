373ccebfca33554805a0ffb87a8482864d2efc7c
Amit Robert Baroi <amitrobertbaroi@gmail.com>
2020-01-20 20:28:54 +0600

    Completed Weed 4 lessons

---
+++ Course 4 - Convolutional Neural Networks/pretrained-model/dl.py
@@ -0,0 +1,5 @@
+import wget
+
+url = "https://eyvhaqwvptqitbawgcfogx.coursera-apps.org/files/week4/Neural%20Style%20Transfer/pretrained-model/imagenet-vgg-verydeep-19.mat?download=1"
+
+wget.download(url)
