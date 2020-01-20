79c09f96beea0d29e8178a272d593e9aa7389d0e
Amit Robert Baroi <amitrobertbaroi@gmail.com>
2020-01-20 20:32:41 +0600

    Added necessary resources

--- Course 4 - Convolutional Neural Networks/pretrained-model/dl.py
+++
@@ -1,5 +0,0 @@
-import wget
-
-url = "https://eyvhaqwvptqitbawgcfogx.coursera-apps.org/files/week4/Neural%20Style%20Transfer/pretrained-model/imagenet-vgg-verydeep-19.mat?download=1"
-
-wget.download(url)
